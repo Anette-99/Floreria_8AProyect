55e5de1247868c7aeed6a9b696f366d36e89c9eb
Anette-99 <ascencio80998@gmail.com>
2021-05-05 14:08:59 -0500

    RegisterForm

---
+++ app/components/Account/RegisterForm.js
@@ -0,0 +1,71 @@
+import React, {useState} from 'react'
+import { StyleSheet, View, Text } from 'react-native'
+import { Input, Icon, Button } from 'react-native-elements'
+
+export default function RegisterForm(){
+    const [showPassword, setShowPassword] = useState(false)
+    const [showRepeatPassword, setShowRepeatPassword] = useState(false)
+
+    return(
+        <View style={styles.formContainer}>
+            <Input
+                placeholder='Correo electronico'
+                containerStyle={styles.inputForm}
+                rightIcon={<Icon 
+                    type='material-community' 
+                    name='at' 
+                    iconStyle={styles.iconRight}
+                />}
+            />
+            <Input
+                placeholder='Contraseña'
+                containerStyle={styles.inputForm}
+                password={true}
+                secureTextEntry={showPassword ? false : true}
+                rightIcon={<Icon 
+                    type='material-community' 
+                    name={showPassword ? 'eye-off-outline' : 'eye-outline' }
+                    iconStyle={styles.iconRight}
+                    onPress={()=> setShowPassword(!showPassword)}
+            />}
+            />
+            <Input
+                placeholder='Repetir Contraseña'
+                containerStyle={styles.inputForm}
+                password={true}
+                secureTextEntry={showRepeatPassword ? false : true}
+                rightIcon={<Icon 
+                    type='material-community' 
+                    name={showRepeatPassword ? 'eye-off-outline' : 'eye-outline' } 
+                    iconStyle={styles.iconRight}
+                    onPress={()=> setShowRepeatPassword(!showRepeatPassword)}
+            />}
+            />
+            <Button
+                title='Únete'
+                containerStyle={styles.btnContainerRegister}
+                buttonStyle={styles.btnRegister}
+            />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    formContainer:{
+        marginTop: 30
+    },
+    inputForm:{
+        width: '100%',
+        marginTop: 20
+    },
+    btnContainerRegister:{
+        marginTop: 20,
+        width: '95%'
+    },
+    btnRegister:{
+        backgroundColor: '#008a81'
+    },
+    iconRight:{
+        color: '#c1c1c1'
+    }
+})
