5e4781d7c7bde6b7c2736a3ae54d92332802a588
Anette-99 <ascencio80998@gmail.com>
2021-05-09 01:27:19 -0500

    Configuración de Firebase

--- app/components/Account/RegisterForm.js
+++ app/components/Account/RegisterForm.js
@@ -2,12 +2,15 @@ import React, {useState} from 'react'
 import { StyleSheet, View, Text } from 'react-native'
 import { Input, Icon, Button } from 'react-native-elements'
 import { validateEmail } from '../../utils/validation'
+import firebase from 'firebase'
+import {useNavigation} from '@react-navigation/native'
 
 export default function RegisterForm(props){
     const {toastRef} = props
     const [showPassword, setShowPassword] = useState(false)
     const [showRepeatPassword, setShowRepeatPassword] = useState(false)
     const [formData, setFormData] = useState(defaultFormValues())
+    const navigation = useNavigation()
 
     const onSubmit = () => {
         if(formData.email.length===0||formData.password.length===0||formData.repeatPassword.length===0){
@@ -43,7 +46,21 @@ export default function RegisterForm(props){
                 visibilityTime: 3000
               });
         } else{
-            console.log('Todo Bien')
+            firebase
+            .auth()
+            .createUserWithEmailAndPassword(formData.email, formData.password)
+            .then((response)=>{
+                navigation.navigate('account')
+            })
+            .catch(()=>{
+                toastRef.current.show({
+                    type: 'error',
+                    position: 'top',
+                    text1: 'Cuenta',
+                    text2: 'Este correo  ya ha sido registrado 👋',
+                    visibilityTime: 3000
+                  });
+            })
         }
         //console.log(formData)
         //console.log(validateEmail(formData.email))
